357e681befdd82e80b355dd75f43c7e736b23b21
Rikoze777 <spawn00007@gmail.com>
2023-07-15 21:39:27 +0500

    add shot

--- main.py
+++ main.py
@@ -7,18 +7,59 @@ import random
 TIC_TIMEOUT = 0.1
 
 
+async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0):
+    """Display animation of gun shot, direction and speed can be specified."""
+
+    row, column = start_row, start_column
+
+    canvas.addstr(round(row), round(column), '*')
+    await asyncio.sleep(0)
+
+    canvas.addstr(round(row), round(column), 'O')
+    await asyncio.sleep(0)
+    canvas.addstr(round(row), round(column), ' ')
+
+    row += rows_speed
+    column += columns_speed
+
+    symbol = '-' if columns_speed else '|'
+
+    rows, columns = canvas.getmaxyx()
+    max_row, max_column = rows - 1, columns - 1
+
+    curses.beep()
+
+    while 0 < row < max_row and 0 < column < max_column:
+        canvas.addstr(round(row), round(column), symbol)
+        await asyncio.sleep(0)
+        canvas.addstr(round(row), round(column), ' ')
+        row += rows_speed
+        column += columns_speed
+
+
 def draw(canvas):
     canvas.border()
     star_sprites = '+*.:'
     window_rows, window_columns = canvas.getmaxyx()
+    shot = fire(canvas, window_rows-1,
+                window_columns/2,
+                rows_speed=-0.3,
+                columns_speed=0)
     coroutines = [blink(canvas,
                         row=random.randint(2, window_rows-2),
                         column=random.randint(2, window_columns-2),
-                        symbol=random.choice(star_sprites)) for _ in range(100)]
+                        symbol=random.choice(star_sprites))
+                  for _ in range(100)]
     curses.curs_set(False)
     while True:
-        for coroutine in coroutines:
-            coroutine.send(None)
+        shot.send(None)
+        # try:
+        #     for coroutine in coroutines.copy():
+        #         coroutine.send(None)
+        # except StopIteration:
+        #     coroutines.remove(coroutine)
+        # if len(coroutines) == 0:
+        #     break
         canvas.refresh()
         time.sleep(TIC_TIMEOUT)
 
